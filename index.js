f1cf3a8231182fcbd4671a8623360fe3cb15dd63
joshcoder2000 <jdd1993@hotmail.com>
2024-08-10 17:02:35 -0300

    Updates cors origins

--- index.js
+++ index.js
@@ -32,9 +32,7 @@ app.use(
 	cors({
 		origin: [
 			"https://blog-api-users-page.vercel.app",
-			"https://blog-api-admin-page.vercel.app",
-			"http://localhost:5173",
-			"http://localhost:5174"
+			"https://blog-api-admin-page.vercel.app"
 		],
 		credentials: true
 	})
