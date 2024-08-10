b95215644b3f08c7005edd44637f4cdd11a2e25f
joshcoder2000 <jdd1993@hotmail.com>
2024-08-10 16:32:34 -0300

    Updates cors origins

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ db.on("connected", console.log.bind(console, "connected to database"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
+app.use(cors());
+/* app.use(
 	cors({
 		origin: [
 			"https://blog-api-users-page.vercel.app",
@@ -37,7 +38,7 @@ app.use(
 		],
 		credentials: true
 	})
-);
+); */
 
 app.use("/", indexRouter);
 app.use("/user", userRouter);
